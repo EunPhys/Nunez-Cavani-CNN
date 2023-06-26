4c7013ac4dc4bf500cc3fb06e873d5cb88c3074b
Eunan <98047928+EunPhys@users.noreply.github.com>
2023-06-26 14:49:50 +0100

    Update NunCavCNN.py

--- NunCavCNN.py
+++ NunCavCNN.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras import layers, models
